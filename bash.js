544ad588b99d69c71d5c0c3c11b55d9b621dd6cd
Mikhail Qader <mhq2101@columbia.edu>
2017-06-12 11:30:50 -0400

    command.js linked

--- bash.js
+++ bash.js
@@ -1,26 +1,27 @@
 // Output a prompt
-// process.stdout.write('prompt > ');
+process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
 
 
-  if (cmd === 'pwd') {
-      process.stdout.write(process.argv[1]);
-  }
-  else if (cmd === 'date') {
-   var today = new Date();
+//   if (cmd === 'pwd') {
+//       process.stdout.write(process.argv[1]);
+//   }
+//   else if (cmd === 'date') {
+//    var today = new Date();
 
-    process.stdout.write(today.toString());
-  }
-  else {
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
-  }
+//     process.stdout.write(today.toString());
+//   }
+//   else {
+//     process.stdout.write('You typed: ' + cmd);
+//     process.stdout.write('\nprompt > ');
+//   }
+
+var commands = require('./commands');
+// var userCommand = 'pwd';
+commands[cmd]();
 
 });
 
-var commands = require('./commands');
-var userCommand = 'pwd';
-commands[userCommand]();
--- commands.js
+++ commands.js
@@ -1,14 +1,12 @@
-process.stdout.write('prompt > ');
+
 
 module.exports = {
   pwd: function() {
-      process.stdin.on('data', function (data) {
-        var cmd = data.toString().trim(); // remove the newline
-
-        process.stdout.write(process.argv[1]);
-      }
-  };
+    process.stdout.write(process.argv[1]);
+    process.stdout.write('\nprompt > ');
+  }
 }
+
 
 
 
